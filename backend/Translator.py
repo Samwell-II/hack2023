9c6593c90fed063c8c89d4b1a42b698cb904ddea
Samwell <samlikesgreeneggs@gmail.com>
2023-03-25 11:49:25 -0600

    checking in

--- backend/Translator.py
+++ backend/Translator.py
@@ -1,5 +1,6 @@
 import LinearAlgebra as LA
 import LPSolver as LP
+import helper as h
 
 def indSets(A):
     n=len(A)
@@ -24,6 +25,71 @@ def indSetsHelp(A, s):
     s2[i]=0
     return [] + indSetsHelp(A,s1) + indSetsHelp(A,s2)
 
+def evIncidence(A):
+    I = []
+    n=len(A)
+    # string = ""
+    for i in range(n):
+        for j in range(i):
+            # string += str(A[i][j])+"\t"
+            if A[i][j] == 1:
+                c=[0]*n
+                c[i]=1
+                c[j]=1
+                I+= [c]
+        # string += "\n"
+    # print(string)
+    return I
+
+def biqedgIncidence(A):
+    n = len(A)
+    p=[]
+    bicliques = []
+    q = h.Queue()
+    # prev = set()
+    for i in range(n):
+        pi = []
+        for j in range(n):
+            if A[i][j]==1:
+                pi.append(j)
+        p.append(set(pi))
+        # pi = p[i].difference(prev)
+        # if len(pi)!=0:
+        q.add(len(p[i]),({i},p[i],i+1))
+        # prev.add(i)
+
+    print()
+
+    while not q.isEmpty():
+        print(q)
+        data = q.pop()
+        print(data)
+        # print(data)
+        d0 = data[0]
+        d1 = data[1]
+        d2 = data[2]
+
+        if d2 >= n:
+            bicliques.append(data)
+            continue
+        q.add(len(d1),(d0,d1,d2+1))
+        newD1 = d1.intersection(p[d2])
+        # print("newD1==" + str(newD1))
+        if len(newD1) != 0:
+            d0.add(d2)
+            q.add(len(newD1),(d0.copy(),newD1,d2+1))
+        print()
+    
+    print(bicliques)
+
+    
+            
+            
+    
+    
+    
+
+
 A = [
     [0,1,1],
     [1,0,0],
@@ -38,4 +104,5 @@ B = [
     [1,0,0,1,0]
 ]
 
-print(indSets(B))
+# print(evIncidence(B))
+biqedgIncidence(B)
---
+++ backend/helper.py
@@ -0,0 +1,70 @@
+
+class Queue:
+    root = None
+
+    def add(self, i, data):
+        n = QueueVertex(i, data)
+        print("adding " + str(n))
+        if self.root is None:
+            self.root = n
+        else:
+            self.root = self.root.add(n)
+        return self
+
+    def pop(self):
+        if self.root is None:
+            return None
+        oldroot = self.root
+        if oldroot.right is not None:
+            self.root = oldroot.right.add(oldroot.left)
+        else:
+            self.root = None
+        return oldroot.data
+
+    def isEmpty(self):
+        return self.root is None
+
+    def __str__(self):
+        if self.root is None:
+            return "Empty Queue"
+        return str(self.root)
+
+class QueueVertex:
+    def __init__(self, i, data):
+        self.i = i
+        self.data = data
+        self.right = None
+        self.left = None
+
+    def add(self, n):
+        if n is None:
+            return self
+        if self.i > n.i:
+            return n.add(self)
+        if self.right == None:
+            self.right = n
+            return self
+        if self.left == None:
+            if self.right.i > n.i:
+                self.left = self.right
+                self.right = n
+                return self
+            else:
+                self.left = n
+                return self
+        self.left = self.left.add(n)
+        return self
+
+    def __str__(self):
+        string = "<" + str(self.i) + ">" + str(self.data)
+        if self.right is not None:
+            string += ", " + str(self.right)
+        if self.left is not None:
+            string += ", " + str(self.left)
+        return string
+
+# q = Queue()
+# print(q.isEmpty())
+# q.add(1,12).add(2,12).add(31,(1,2,3)).add(4,5).add(-1,"string type").add(0,0)
+# while not q.isEmpty():
+#     print(q.pop())
